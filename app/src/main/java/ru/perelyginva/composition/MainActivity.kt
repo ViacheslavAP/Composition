f3ecb213cade0a7fbb056068571c649383f9c4ee
ViacheslavA <perelyginva@gmail.com>
2023-06-17 11:44:40 +0300

    Create Project

---
+++ app/src/main/java/ru/perelyginva/composition/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.perelyginva.composition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.perelyginva.composition.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
